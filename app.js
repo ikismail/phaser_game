6a8868d73e6bf2b1c94f66c18fe57bccf321d4a0
ikismail <ikismail7@gmail.com>
2018-03-02 18:18:26 +0530

    Added character to the game

--- app.js
+++ app.js
@@ -36,7 +36,9 @@ function preload() {
 
 // executed after everyting is loaded
 
-var platforms;
+var platforms,player;
+
+
 function create() {
     this.background = this.add.image(400, 300, 'sky')
 
@@ -47,6 +49,32 @@ function create() {
     platforms.create(600, 400, 'platform');
     platforms.create(50, 250, 'platform');
     platforms.create(750, 220, 'platform');
+
+
+    player = this.physics.add.sprite(100, 450, 'dude');
+
+    player.setBounce(0.2);
+    player.setCollideWorldBounds(true);
+
+    this.anims.create({
+        key: 'left',
+        frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
+        frameRate: 10,
+        repeat: -1
+    });
+
+    this.anims.create({
+        key: 'turn',
+        frames: [ { key: 'dude', frame: 4 } ],
+        frameRate: 20
+    });
+
+    this.anims.create({
+        key: 'right',
+        frames: this.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
+        frameRate: 10,
+        repeat: -1
+    });
 }
 
 // this is executed multiple times per second
